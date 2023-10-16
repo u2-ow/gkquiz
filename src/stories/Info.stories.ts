e284b1b5662ba436691188be4d41f8dcb39b65bc
u2-ow <utwo4805@gmail.com>
2023-10-16 22:26:23 +0900

    :construction: コンポーネントカタログを作成

---
+++ src/stories/Info.stories.ts
@@ -0,0 +1,9 @@
+import Info from "./Info";
+
+export default {
+    component: Info,
+    title: 'Info',
+};
+
+export const infoBox ={};
+
---
+++ src/stories/Info.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './info.css';
+
+const Info = () => {
+  return (
+    <div className='info'>
+        <p className='infoHead'>Info</p>
+        <div className='infoInner'>
+            <p className='infoInnerTitle'>遊び方</p>
+            <p className='infoInnerText'>迫ってくる一般常識クイズタイピングで<br></br>答えるゲーム！<br></br>問題が迫りきったら(仮)GAME OVERになります。</p>
+        </div>
+    </div>
+  )
+}
+
+export default Info
