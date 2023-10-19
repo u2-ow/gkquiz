fe9141acddcb8d51088ee19fc9f978035a3ef8af
u2-ow <utwo4805@gmail.com>
2023-10-19 22:53:28 +0900

    :construction: 諸々調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
-import Image from 'next/image'
+import LevelButton from '@/components/LevelButton'
+import Styles from '@/styles/page.module.scss'
+
+import { Paytone_One } from 'next/font/google'
+const payone = Paytone_One({
+  subsets: ['latin'],
+  weight: '400'
+})
 
 
 export default function Home() {
   return (
     <>
-    <h1>Hello world</h1>
+    <main className={Styles.mainArea}>
+      <h1 className={Styles.mainAreaTitle}>gkquizz<span className={Styles.mainAreaTitleSub}>一般常識クイズ</span></h1>
+      <p className={Styles.mainAreaText}>クイズに答えて一般常識を身につけよう!</p>
+      <LevelButton className='beginnerBtn levelBtn'>初級</LevelButton>
+      <LevelButton className='intermediateBtn levelBtn'>中級</LevelButton>
+      <LevelButton className='advancedBtn levelBtn'>上級</LevelButton>
+    </main>
+
     </>
   )
 }
