798e6d6fe834a19c5f597d7daaa6ea6528bb2ff2
u2-ow <utwo4805@gmail.com>
2023-11-04 15:59:47 +0900

    :fire: 不要になったので削除

--- prisma/migrations/20231028155521_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Post" (
-    "id" SERIAL NOT NULL,
-    "questionTitle" TEXT NOT NULL,
-    "questionAnswer" TEXT NOT NULL,
-
-    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
-);
