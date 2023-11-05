ece10232e6fb7b90af9f54c34f2b485c14530586
u2-ow <utwo4805@gmail.com>
2023-11-05 16:30:11 +0900

    questionTitleを取得できるようにした

---
+++ src/app/japanese/page.tsx
@@ -0,0 +1,31 @@
+"use client"
+import { QuestionType } from "@/types/type";
+import { useEffect, useState } from "react";
+
+
+
+export default function App() {
+  const [japaneseQuestion,setJapaneseQuestion] =  useState([]);
+  useEffect(() => {
+    async function fetchAllJapaneseData() {
+      const res = await fetch('http://localhost:3000/api/japanese',{
+        cache: "no-store",//SSR
+      });
+      const data = await res.json();
+      const dataArray = data.posts;
+      const dataArrayQuesitonTitle = dataArray.map((item:QuestionType)=>{
+        console.log(item.questionTitle)
+      })
+      // console.log('HAHAHAH')
+      // console.log(dataArrayItem)
+    }
+    fetchAllJapaneseData();
+  },[]);
+
+  return (
+    <div>
+      <p>{japaneseQuestion}</p>
+      <h1>日本語の問題</h1>
+    </div>
+  );
+}
