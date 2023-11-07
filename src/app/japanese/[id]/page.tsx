16db3deca4732202e27c657d33db65dc691dda51
u2-ow <utwo4805@gmail.com>
2023-11-07 23:17:33 +0900

    :construction: 国語のページを諸々作業

--- src/app/japanese/[id]/page.tsx
+++ src/app/japanese/[id]/page.tsx
@@ -2,13 +2,17 @@
 import FormInput from "@/components/FormInput";
 import { QuestionType } from "@/types/type";
 import { useEffect, useState } from "react";
+import { counterAtom } from "@/jotail/atoms";
 
-import Styles from "@/app/japanese/page.module.scss"
+import Styles from "@/app/japanese/[id]/page.module.scss"
+import { useAtom } from "jotai";
 
 
 
 export default function App() {
   const [japaneseQuestion,setJapaneseQuestion] =  useState([]);
+  const [count, setCount] = useAtom(counterAtom);
+
   useEffect(() => {
     async function fetchAllJapaneseData() {
       const res = await fetch('http://localhost:3000/api/japanese',{
@@ -30,8 +34,10 @@ export default function App() {
   return (
     <div>
       <p className={Styles.questionNumber}>第　問</p>
+      <p>{count}</p>
       <p className={Styles.questionTitle}>{japaneseQuestion}</p>
       <FormInput/>
+
     </div>
   );
 }
