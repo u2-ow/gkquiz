e61fc1e59e8d8f4b6a371447f42e140d764d47de
u2-ow <utwo4805@gmail.com>
2023-11-09 22:37:31 +0900

    :fire: 不要なコードを削除

--- src/app/japanese/[id]/page.tsx
+++ src/app/japanese/[id]/page.tsx
@@ -2,16 +2,18 @@
 import FormInput from "@/components/FormInput";
 import { QuestionType } from "@/types/type";
 import { useEffect, useState } from "react";
-import { counterAtom } from "@/jotail/atoms";
+import { userInputAtom } from "@/jotail/atoms";
 
 import Styles from "@/app/japanese/[id]/page.module.scss"
-import { useAtom } from "jotai";
+
 
 
 
 export default function App() {
   const [japaneseQuestion,setJapaneseQuestion] =  useState([]);
-  const [count, setCount] = useAtom(counterAtom);
+
+
+
 
   useEffect(() => {
     async function fetchAllJapaneseData() {
@@ -34,7 +36,7 @@ export default function App() {
   return (
     <div>
       <p className={Styles.questionNumber}>第　問</p>
-      <p>{count}</p>
+      {/* <p>{count}</p> */}
       <p className={Styles.questionTitle}>{japaneseQuestion}</p>
       <FormInput/>
 
