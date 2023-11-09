484a4e3f2c2c2cb5dbe219d0f8e65d8eda0b5903
u2-ow <utwo4805@gmail.com>
2023-11-09 22:38:14 +0900

    :construction: ユーザーが回答した際のinputの挙動を整えた

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Image from "next/image";
 import Styles from "@/components/FormInput.module.scss"
 
+import { useAtom } from "jotai";
+import { userInputAtom } from '@/jotail/atoms';
+
 export default function FormInput() {
+
+const inputRef = useRef<HTMLInputElement>(null)
+const inputRefValue = inputRef.current as HTMLInputElement;
+
+/*グローバルステート(ユーザーが回答した内容を保存する) */
+const [userInput, setUserInput] = useAtom(userInputAtom);
+
+
+
+
+
+const postAnswer = (e: { preventDefault: () => any; })=>{
+    console.log(userInput)
+    inputRefValue.value = "";
+    return e.preventDefault();
+}
+
+
+
+
   return (
+    
     <>
-        <form action="" className={Styles.answerForm}>
-            <input type="text" inputMode="text" placeholder="Enterまたは改行で回答" className={Styles.answerFormInput}  />
+        <form action="" onSubmit={postAnswer} className={Styles.answerForm}>
+            <input value={userInput} ref={inputRef} onChange={(e) => setUserInput(e.target.value)} inputMode="text" placeholder="Enterまたは改行で回答" className={Styles.answerFormInput} />
             <Image
             src="/heart.svg"
             width={30}
